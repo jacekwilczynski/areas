b5158addb97fc90536fbfe2ffb12915dc171a823
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2020-04-04 09:24:10 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
 const buttons = Array.from(document.querySelectorAll('[data-add-shape]'));
 
+window.shapes = [];
+
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    if (e.target.dataset.addShape == 'circle') {
+    const shapeType = e.target.dataset.addShape;
+    const shape = { type: shapeType };
+
+    if (shapeType == 'square') {
+      const side = prompt('Enter square side size:');
+      if (!side) {
+        return;
+      }
+      shape.side = side;
     }
+
+    if (shapeType == 'rectangle') {
+      const width = prompt('Enter rectangle width:');
+      if (!width) {
+        return;
+      }
+      const height = prompt('Enter rectangle height:');
+      if (!height) {
+        return;
+      }
+      shape.width = width;
+      shape.height = height;
+    }
+
+    if (shapeType == 'circle') {
+      const radius = prompt('Enter circle radius:');
+      if (!radius) {
+        return;
+      }
+      shape.radius = radius;
+    }
+
+    shapes.push(shape);
   });
 });
